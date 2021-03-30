dc84ec055cc448808c071399fa7fd168bb4d6bf7
Niraj <nirajprane@gmail.com>
2021-03-30 16:41:49 +0530

    added signup controller

--- spring/src/main/java/com/example/controller/SignupController.java
+++ spring/src/main/java/com/example/controller/SignupController.java
@@ -1,17 +1,31 @@
 package com.example.controller;
 
+import com.example.model.UserModel;
+import com.example.repository.UserRepo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * SignupController
- */
+
 @CrossOrigin(origins = "http://localhost:3000")     //change it later
 @RestController
 @RequestMapping("/api/vi/")
 public class SignupController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @PostMapping("/signup")
+    public UserModel saveUser(@RequestBody UserModel userModel){
+        return userRepo.save(userModel);
+
+    }
+
+
 
     
     

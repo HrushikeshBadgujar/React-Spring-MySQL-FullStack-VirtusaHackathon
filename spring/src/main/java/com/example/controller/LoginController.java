76f2fc00e0ac585751e61977969bd3c08c279758
Niraj <nirajprane@gmail.com>
2021-03-30 17:11:48 +0530

    added login controller

---
+++ spring/src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import com.example.repository.LoginRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")     //change it later
+@RestController
+@RequestMapping("/api/v1/")
+
+
+public class LoginController {
+
+    @Autowired
+    private LoginRepo loginRepo;
+
+    //@PostMapping("/login")
+    
+
+
+    
+
+    
+}

47e8574f8e42cd456a8628e1422606d126cf208d
HRUSHI003 <hrushibadgujar003@gmail.com>
2021-04-04 17:43:05 +0530

    react login signup with security

--- react/src/components/NavBar.js
+++ react/src/components/NavBar.js
@@ -1,97 +1,190 @@
-import React, { Component, Fragment } from 'react';
-import {Link} from "react-router-dom";
-import { Container } from 'reactstrap';
-import tap from '../images/tap.jfif';
-import { Navbar,Nav,NavDropdown } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavbarText, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function NavBar(){
-return (
-    <Fragment>
-        <div style={{marginBottom:"50px",width:"100% !important"}}>
-    <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark">
-    <Navbar.Brand href="#home">WMP</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
+import { withRouter } from 'react-router-dom';
+
+import AuthenticationService from '../Services/AuthenticationService';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isOpen: false};
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      showUser: false,
+      showAdmin: false,
+      username: undefined,
+      login: false
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthenticationService.getCurrentUser();
+
+    if (user) {
+      const roles = [];
+
+      user.authorities.forEach(authority => {
+        roles.push(authority.authority)
+      });
+  
+      this.setState({
+        showUser: true,
+        showAdmin: roles.includes("ROLE_ADMIN"),
+        login: true,
+        username: user.username
+      });
+    }
+  }
+
+  signOut = () => {
+    AuthenticationService.signOut();
+    this.props.history.push('/home');
+    window.location.reload();
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return <Navbar color="dark" dark expand="md">
+      <NavbarBrand tag={Link} to="/login">WMP</NavbarBrand>
       <Nav className="mr-auto">
-        <Nav.Link href="/login">Login</Nav.Link>
-        <Nav.Link href="/signup">Signup</Nav.Link>
-        <NavDropdown title="Water Info" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="/addInfo">Add Water Info</NavDropdown.Item>
-          {/*  */}
-          <NavDropdown.Item href="/GetWaterInfo/:id">Update Water Info</NavDropdown.Item>
-        </NavDropdown>
-        
-        {/* Admin Section Checking */}
-        <NavDropdown title="Admin" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="/admin">All Water Info</NavDropdown.Item>
-          <NavDropdown.Item href="/admin/feedback">All Feedback</NavDropdown.Item>
-          <NavDropdown.Item href="/admin/feedback/:id">Feedback by Id</NavDropdown.Item>
-        </NavDropdown>
-
+        <NavLink href="/home">Home</NavLink>
+        {this.state.showUser && <NavLink href="/user">User</NavLink>}
+        {this.state.showAdmin && <NavLink href="/admin">Admin</NavLink>}
       </Nav>
-
-
-      <Nav>
-        <Nav.Link href="/AddFeedback">Add Feedback</Nav.Link>
-        <Nav.Link href="/GetFeedback">Get Feedback</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-  </div>
-</Fragment>
-)
+      <NavbarToggler onClick={this.toggle}/>
+      <Collapse isOpen={this.state.isOpen} navbar>
+        {
+          this.state.login ? (
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                  <NavbarText>
+                    Signed in as: <a href="/profile">{this.state.username}</a>
+                  </NavbarText>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#" onClick={this.signOut}>SignOut</NavLink>
+              </NavItem>
+            </Nav>                 
+          ) : (
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/login">Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/signup">Register</NavLink>
+              </NavItem>
+            </Nav>
+          )
+        }
+      </Collapse>
+    </Navbar>;
+  }
 }
-export default NavBar;
+
+export default withRouter(NavBar);
+
+// import React, { Component, Fragment } from 'react';
+// import {Link} from "react-router-dom";
+// import { Container } from 'reactstrap';
+// import tap from '../images/tap.jfif';
+// import { Navbar,Nav,NavDropdown } from 'react-bootstrap';
+
+// function NavBar(){
+// return (
+//     <Fragment>
+//         <div style={{marginBottom:"50px",width:"100% !important"}}>
+//     <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark">
+//     <Navbar.Brand href="#home">WMP</Navbar.Brand>
+//     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//     <Navbar.Collapse id="responsive-navbar-nav">
+//       <Nav className="mr-auto">
+//         <Nav.Link href="/login">Login</Nav.Link>
+//         <Nav.Link href="/signup">Signup</Nav.Link>
+//         <NavDropdown title="Water Info" id="collasible-nav-dropdown">
+//           <NavDropdown.Item href="/addInfo">Add Water Info</NavDropdown.Item>
+//           {/*  */}
+//           <NavDropdown.Item href="/GetWaterInfo/:id">Update Water Info</NavDropdown.Item>
+//         </NavDropdown>
+        
+//         {/* Admin Section Checking */}
+//         <NavDropdown title="Admin" id="collasible-nav-dropdown">
+//           <NavDropdown.Item href="/admin">All Water Info</NavDropdown.Item>
+//           <NavDropdown.Item href="/admin/feedback">All Feedback</NavDropdown.Item>
+//           <NavDropdown.Item href="/admin/feedback/:id">Feedback by Id</NavDropdown.Item>
+//         </NavDropdown>
+
+//       </Nav>
 
 
-
-// import React, { Component } from 'react';
-// import { Container, Row, Col } from 'reactstrap';
-// import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-
-
-// class NavBar extends Component {
-//   render() {
-//     return (
-//       <Container>
-//         <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark">
-//           <Navbar.Brand href="#home">WMP</Navbar.Brand>
-//           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//           <Navbar.Collapse id="responsive-navbar-nav">
-//             <Nav className="mr-auto">
-//               <Nav.Link href="#features">Login</Nav.Link>
-//               <Nav.Link href="#pricing">Register</Nav.Link>
-//               <NavDropdown title="Water Info" id="collasible-nav-dropdown">
-//                 <NavDropdown.Item href="#action/3.1">Add Water Info</NavDropdown.Item>
-//                 <NavDropdown.Item href="#action/3.2">Update Water Info</NavDropdown.Item>
-//               </NavDropdown>
-//             </Nav>
-//             <Nav>
-//               <Nav.Link href="#deets">Feedback</Nav.Link>
-//             </Nav>
-//           </Navbar.Collapse>
-//         </Navbar>
-
-//         <Row>
-//           <Col>
-//             <Header />
-//           </Col>
-//         </Row>
-
-//         <Row>
-//           <Col md={4}>
-//             <Register />
-//           </Col>
-//         </Row>
-
-//         <Row>
-//           <Col>
-//             <Footer />
-//           </Col>
-//         </Row>
-//       </Container>
-//     );
-//   }
+//       <Nav>
+//         <Nav.Link href="/AddFeedback">Add Feedback</Nav.Link>
+//         <Nav.Link href="/GetFeedback">Get Feedback</Nav.Link>
+//       </Nav>
+//     </Navbar.Collapse>
+//   </Navbar>
+//   </div>
+// </Fragment>
+// )
 // }
-
 // export default NavBar;
+
+
+
+// // import React, { Component } from 'react';
+// // import { Container, Row, Col } from 'reactstrap';
+// // import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+
+// // class NavBar extends Component {
+// //   render() {
+// //     return (
+// //       <Container>
+// //         <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark">
+// //           <Navbar.Brand href="#home">WMP</Navbar.Brand>
+// //           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+// //           <Navbar.Collapse id="responsive-navbar-nav">
+// //             <Nav className="mr-auto">
+// //               <Nav.Link href="#features">Login</Nav.Link>
+// //               <Nav.Link href="#pricing">Register</Nav.Link>
+// //               <NavDropdown title="Water Info" id="collasible-nav-dropdown">
+// //                 <NavDropdown.Item href="#action/3.1">Add Water Info</NavDropdown.Item>
+// //                 <NavDropdown.Item href="#action/3.2">Update Water Info</NavDropdown.Item>
+// //               </NavDropdown>
+// //             </Nav>
+// //             <Nav>
+// //               <Nav.Link href="#deets">Feedback</Nav.Link>
+// //             </Nav>
+// //           </Navbar.Collapse>
+// //         </Navbar>
+
+// //         <Row>
+// //           <Col>
+// //             <Header />
+// //           </Col>
+// //         </Row>
+
+// //         <Row>
+// //           <Col md={4}>
+// //             <Register />
+// //           </Col>
+// //         </Row>
+
+// //         <Row>
+// //           <Col>
+// //             <Footer />
+// //           </Col>
+// //         </Row>
+// //       </Container>
+// //     );
+// //   }
+// // }
+
+// // export default NavBar;
---
+++ react/src/components/User/Login.js
@@ -0,0 +1,199 @@
+import React, { Component } from 'react';
+import image from '../../images/water.jfif'
+import { Form, Input, Label, FormGroup, FormFeedback, Button, Alert } from 'reactstrap';
+import {Container} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+
+import Navbar from '../NavBar';
+
+import AuthenticationService from "../../Services/AuthenticationService";
+
+import UserServices from '../../Services/UserServices'
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: "",
+            password: "",
+            error: ""
+          };
+    }
+
+    // getInitialState = () => ({
+    //     data: {
+    //         userid: '',
+    //         password: ''
+    //     },
+    //     errors: {}
+    // });
+
+    // componentDidMount(){
+    //     UserServices.checkUser(this.state.userid).then( (res) =>{
+    //         let UserTable = res.data;
+    //         this.setState({
+            
+    //             password : UserTable.password,
+    //             email : UserTable.email,
+    //         });
+    //     });
+    // }
+
+    changeHandler = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+      }
+    
+      doLogin = async (event) => {
+        event.preventDefault();
+    
+        AuthenticationService
+            .signin(this.state.username, 
+                      this.state.password)
+          .then(
+            () => {
+              this.props.history.push('/profile');
+            },
+            error => {
+              console.log("Login fail: error = { " + error.toString() + " }");
+              this.setState({error: "Can not signin successfully ! Please check username/password again"});
+            }
+        );
+      }
+
+    // handleChange = (e) => {
+    //     this.setState({
+    //         data: {
+    //             ...this.state.data,
+    //             [e.target.name]: e.target.value
+    //         },
+    //         errors: {
+    //             ...this.state.errors,
+    //             [e.target.name]: ''
+    //         }
+    //     });
+    // }
+
+    // validate = () => {
+    //     const { data } = this.state;
+    //     let errors = {};
+
+    //     if (data.userid === '') errors.userid = 'UserID can not be blank.';
+    //     if (data.password === '') errors.password = 'Password must be valid.';
+       
+
+    //     return errors;
+    // }
+
+    // handleSubmit = (e) => {
+    //     e.preventDefault();
+
+    //     const { data } = this.state;
+
+    //     const errors = this.validate();
+
+    //     if (Object.keys(errors).length === 0) {
+    //         console.log(data);
+    //         //Call an api here
+    //         //Resetting the form
+    //         this.setState(this.getInitialState());
+    //     } else {
+    //         this.setState({ errors });
+    //     }
+    //  }
+
+    render() {
+        return ( 
+          <div>
+            <Navbar/>
+            <Container fluid>
+              <Row style={{marginTop:"20px"}}>
+              <Col sm="12" md={{ size: 3, offset: 4 }}>
+                
+                <Form onSubmit={this.doLogin}>
+                  <FormGroup>
+                    <Label for="username"><strong>Username</strong></Label>
+                    <Input autoFocus 
+                      type="text"
+                      name="username" id="username"
+                      value={this.state.username}
+                      placeholder="Enter Username"
+                      autoComplete="username"
+                      onChange={this.changeHandler}
+                    />
+                  </FormGroup>
+    
+                  <FormGroup>
+                    <Label for="password"><strong>Password</strong></Label>
+                    <Input type="password" 
+                      name="password" id="password"
+                      value={this.state.password}
+                      placeholder="Enter Password"
+                      autoComplete="password"
+                      onChange={this.changeHandler}
+                    />
+                  </FormGroup>
+    
+                  <Button type="submit" variant="primary" size="lg" block>
+                    Log In
+                  </Button>
+                  {
+                    this.state.error && (
+                      <Alert color="danger">
+                        {this.state.error}
+                      </Alert>
+                    )
+                  }
+                </Form>
+                </Col>
+              </Row>
+            </Container>
+          </div>);
+      
+    // render() {
+    //     const { data, errors } = this.state;
+    //     return (
+
+            
+    //         <Container>
+    //         <Row >
+    //             <Col >
+    //                 <Button className="bg-primary" block><h3>Login</h3></Button>
+    //             </Col>
+    //             <Col sm={4}></Col>
+    //             <Col></Col>
+    //         </Row>
+    //         <br></br>
+    //         <Row>
+    //             <Col sm={8}>
+    //                      <Form onSubmit={this.handleSubmit}>
+    //                      <FormGroup as row>
+    //                          <Col sm={2}><Label for="userid">Username</Label></Col>
+    //                          <Col sm={10}><Input id="userid" value={data.userid} invalid={errors.userid ? true : false} name="userid" onChange={this.handleChange} /></Col>
+    //                          <FormFeedback>{errors.userid}</FormFeedback>
+    //                      </FormGroup>
+
+    //                      <FormGroup as row>
+    //                         <Col sm={2}><Label for="password">Password</Label></Col>
+    //                         <Col sm={10}><Input id="password" value={data.password} type="password" name="password" invalid={errors.password ? true : false} onChange={this.handleChange} /></Col>
+    //                          <FormFeedback>{errors.password}</FormFeedback>
+    //                      </FormGroup>
+
+    //                      <Button color="primary">login</Button>
+    //                  </Form>
+    //             </Col>
+    //             <Col sm={4} >
+    //                 <img src={image} alt="water"/>
+    //             </Col>
+    //         </Row>
+    //      </Container>
+
+
+    //     );
+     }
+}
+
+export default Login;
